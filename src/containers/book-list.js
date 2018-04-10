b2331902d4624cba3d4c38b3ff961bbcf1bc0b0e
allen <allenlai@C02V40SYHTDG.local>
2018-04-10 13:21:32 -0400

    added comments

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,7 @@
 // book list needs to be aware of the state with redux so its a container
-// (smart component) 
+// (smart component)
 import React, { Component } from 'react';
-import { connect } from 'react-redux';  
+import { connect } from 'react-redux';
 import { selectBook } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
@@ -10,7 +10,7 @@ class BookList extends Component {
     renderList() {
         return this.props.books.map( (book) => {
             return (
-                <li 
+                <li
                     key={book.title}
                     onClick={() => this.props.selectBook(book)}
                     className="list-group-item">
@@ -28,6 +28,7 @@ class BookList extends Component {
     }
 }
 
+// takes application state and shows up as props
 // state contains array of books and active book
 function mapStateToProps(state) {
     // whatever returned will show up as props inside of booklist
@@ -36,16 +37,17 @@ function mapStateToProps(state) {
     };
 }
 
-// anything returned from this function will end up as props 
+// anything returned from this function will end up as props
 // on the booklist container.
 function mapDispatchToProps(dispatch) {
-    // whenever select book is called, 
-    // the result should be passed to all the reducers 
-    return bindActionCreators({ selectBook: selectBook}, dispatch);
+    // whenever select book is called,
+    // the result should be passed to all the reducers
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
 
-// promote booklist from a component to a container - 
+// promote booklist from a component to a container -
 // it needs to know about htis new dispatch, select book. make it as a prop
 export default connect(mapStateToProps, mapDispatchToProps) (BookList);
 
-
+// takes a function and component -- makes a container, container is aware of redux state
+// export default connect(mapStateToProps) (BookList);
